90336c1f12d02f846457073680c791f50ff31443
Keks <keks@htmlacademy.ru>
2025-11-15 15:27:09 +0300

    :heavy_check_mark: Сборка #2

---
+++ 2/js/functions.js
@@ -0,0 +1,20 @@
+/*  Задача № 1 ( Функция для  проверки длины строки)*/
+function comparephraseLenght(phrase = '', maxlenght = 1) {
+  return(phrase.length <= maxlenght);
+
+}
+
+comparephraseLenght(' Вновь пришла зима' , 18);
+
+
+/*  Задача № 2(  Фунция проверки является ли строка палиндромом ) */
+const Palindrome = (phrase)=> {
+  phrase = phrase.replaceAll(' ' , '').tolowerCase();
+  let reverseorder = '';
+  for(let i = phrase.length - 1; i >= 0; i--){
+    reverseorder += phrase[i];
+  }
+  return phrase === reverseorder;
+};
+
+Palindrome('топот');
